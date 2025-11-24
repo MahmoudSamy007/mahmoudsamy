14fe42dee649de14b327c591ab00c1c4931b304d
Mahmoud Samy <mahmuodsamy1999@gmail.com>
2025-11-24 15:58:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,7 @@
 var backgroundImg = 
 [
     "assets/background.jpg", "assets/background1.jpg", "assets/background2.jpg", "assets/background3.jpg",
-    "assets/background4.jpg", "assets/background5.jpg", "assets/background6.jpg", "assets/background7.jpg",
-    "assets/background8.jpg", "assets/background9.jpg", "assets/background10.jpg", "assets/background11.jpg",
-    "assets/background12.jpg", "assets/background13.jpg", "assets/background14.jpg", "assets/background15.jpg"
+    "assets/background4.jpg", "assets/background5.jpg", "assets/background6.jpg"
 ]
 var i = 1
 document.getElementById("changeBackground-btn").addEventListener("click", function(){
@@ -23,5 +21,6 @@ document.getElementById("hideAllElemet").addEventListener("click", function(){
     this.style.display = "none"
 
 })
+
 
 
